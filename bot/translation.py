f541608678fb8eb180b24119e7a291ab3bd28bf5
Hemas0308 <81526699+Hemas0308@users.noreply.github.com>
2022-06-18 18:43:02 +0400

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -5,12 +5,13 @@
 class Translation(object):
     
     START_TEXT = """<b>Hey {}!!</b>
-<i>Am Just A Advance Auto Filter Bot....😉
+<i>ஏடகம்-Library ஒரு டிஜிட்டல் காப்பகமும் டிஜிட்டல் நூலகமும் ஆகும்....😉
 
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
+
+ெலிகிராம் குழுமங்களில் மிகவும் தரமான நூல்களை வழங்கும் முதன்மையான ஒரே நூலகம்.! 🔥🔥😝
 
 For More Details Click Help Button Below..
-@CrazyBotsz
+@aedahamlibrary_noolgam
 </i>"""    
     
     HELP_TEXT = """
@@ -48,11 +49,11 @@ For More Details Click Help Button Below..
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/kaimudinjavai">kaimudinjavai</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a href="https://github.com/Hemas0308/Adv-Auto-Filter-Bot">Click Me</a></i>
 """
